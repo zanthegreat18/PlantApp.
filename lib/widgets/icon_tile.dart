84e1016e807a2b364580ccaeb9ac86d064c056e1
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-05-23 01:22:32 +0700

    tampilan untuk icon data

--- lib/widgets/icon_tile.dart
+++ lib/widgets/icon_tile.dart
@@ -9,3 +9,23 @@ class IconTile extends StatelessWidget {
     required this.icon,
     required this.onTap,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          height: 60,
+          width: 60,
+          decoration: BoxDecoration(
+            color: Colors.white10,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Icon(icon, color: const Color(0xFF00FFB3)),
+        ),
+      ),
+    );
+  }
+}
