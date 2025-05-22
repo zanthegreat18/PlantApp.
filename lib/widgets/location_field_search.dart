7c5720a5be6a2062559bdc94ce230a4ff9a4e0e7
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-05-23 01:35:01 +0700

    location field state

--- lib/widgets/location_field_search.dart
+++ lib/widgets/location_field_search.dart
@@ -9,3 +9,14 @@ class LocationSearchField extends StatefulWidget {
   @override
   State<LocationSearchField> createState() => _LocationSearchFieldState();
 }
+
+class _LocationSearchFieldState extends State<LocationSearchField> {
+  late GooglePlace googlePlace;
+  List<AutocompletePrediction> predictions = [];
+  TextEditingController controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    googlePlace = GooglePlace("AIzaSyCdqmGoK2LoGW5p_WS4CYTCdsQI4OKJH80"); 
+  }
