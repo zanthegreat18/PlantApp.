e050b7f5be7c23353810145c21acaca4ba3b04cf
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-05-23 01:31:01 +0700

    class untuk profile

---
+++ lib/screens/profile_page.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
